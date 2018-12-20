ad080fad29f658a7a386bd9d610273e4566f52a4
haze-it <shoou0000@gmail.com>
2018-12-20 20:30:54 +0900

    add activities

--- src/Pages/Activities.tsx
+++ src/Pages/Activities.tsx
@@ -9,11 +9,14 @@ class App extends React.Component {
         <h1>Activities</h1>
 
         <h2>発表履歴、資料</h2>
+        <h4><a href="https://s-dev-talks.connpass.com/event/109908/" target="_blank">s-dev talks 〜サービス開発勉強会〜 大忘年LT大会</a></h4>
+        <p><a href="https://docs.google.com/presentation/d/15XRr4MdMwIxRhVr0fyuk_kouUAZBb1gXCKkD5qe19so/edit?usp=sharing" target="_blank">2018.12.19 バックエンドエンジニアがフロントエンドを1ヶ月やって見えたもの</a></p>
+
         <h4><a href="https://wakateweb.connpass.com/event/84075/" target="_blank">第13回若手Webエンジニア交流会 #wakateweb</a></h4>
-        <p><a href="https://docs.google.com/presentation/d/1ZZdNF547eQrLujMaMOkWcnTnzg_oemTToI_K2Asq3yk/edit?usp=sharing" target="_blank">「組織と技術選定のおはなし」</a></p>
+        <p><a href="https://docs.google.com/presentation/d/1ZZdNF547eQrLujMaMOkWcnTnzg_oemTToI_K2Asq3yk/edit?usp=sharing" target="_blank">2018.11.13 組織と技術選定のおはなし</a></p>
 
         <h4><a href="https://wakateweb.connpass.com/event/84075/" target="_blank">だいたい新卒エンジニア交流会 vol.16</a></h4>
-        <p>「LINE Messaging APIを使ったサービスを作るときのポイントとつらみ」（資料未公開）</p>
+        <p>2018.09.22 LINE Messaging APIを使ったサービスを作るときのポイントとつらみ（資料非公開）</p>
       </div>
     );
   }
